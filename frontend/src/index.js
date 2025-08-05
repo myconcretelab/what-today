b2e3f3c4269a4a94b5045e54340c94c7f2de1b62
Jacqmin Sébastien <seb@tellthem.be>
2025-08-05 11:26:29 +0200

    feat(frontend): add mui theme provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import dayjs from 'dayjs';
 import 'dayjs/locale/fr';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
 
 // Application configurée en français
 // (formats de date, noms des jours...)
 dayjs.locale('fr');
 
+const theme = createTheme();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>
+);
