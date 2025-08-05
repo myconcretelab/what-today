0e18e196b2d2cf05b4237145e2f7dcd5514d615c
Jacqmin Sébastien <seb@tellthem.be>
2025-08-05 12:15:29 +0200

    mise en prod locale

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 // Appels au backend pour récupérer les arrivées
 export async function fetchArrivals() {
-  const res = await fetch('/api/arrivals');
+  const res = await fetch('http://localhost:3001/api/arrivals');
+  console.log('fetchArrivals called');
   if (!res.ok) throw new Error('HTTP ' + res.status);
+  console.log('fetchArrivals response:', res);
   return res.json();
 }
