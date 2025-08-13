9f0a0036bc8611739e1d9859a5b1545096335816
Jacqmin Sébastien <seb@tellthem.be>
2025-08-13 08:07:37 +0200

    retrait de l'icalendar Airbnb la grée 2 personnes

--- backend/server.js
+++ backend/server.js
@@ -61,8 +61,7 @@ const GITES = [
     couleur: '#3949AB', // bleu
     sources: [
       { url: 'http://www.abritel.fr/icalendar/3d33e48aeded478f8c11deda36f20008.ics?nonTentative', type: 'Abritel' },
-      { url: 'https://www.airbnb.fr/calendar/ical/16674752.ics?s=54a0101efa1112c86756ed2184506173', type: 'Airbnb' },
-      { url: 'https://www.airbnb.fr/calendar/ical/1256595615494549883.ics?s=61ea920c4f6392380d88563f08adcfee', type: 'Airbnb' }
+      { url: 'https://www.airbnb.fr/calendar/ical/16674752.ics?s=54a0101efa1112c86756ed2184506173', type: 'Airbnb' }
     ]
   },
   {
