95ed3957611041efa270917ede496b5ce8fc333f
Sebastien Jacqmin <seb@tellthem.be>
2026-02-28 19:44:27 +0100

    feat: enhance update script to check and handle package-lock.json changes

--- update.sh
+++ update.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
+before_lock_hash='';
+if [[ -f package-lock.json ]]; then
+  before_lock_hash="$(shasum package-lock.json | awk '{print $1}')";
+fi
+
 # Synchronize repository with remote
 git pull
+
+after_lock_hash='';
+if [[ -f package-lock.json ]]; then
+  after_lock_hash="$(shasum package-lock.json | awk '{print $1}')";
+fi
+
+if [[ "${before_lock_hash}" != "${after_lock_hash}" ]]; then
+  npm ci
+fi
 
 # Build the frontend application
 npm run build
